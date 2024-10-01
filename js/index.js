a58ce6bd18adba5f520c5f4312262aae8cf7ed61
Muhammad Ranju <mdranju23@gmail.com>
2024-10-01 22:19:42 +0600

    Add category filter to blog posts

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-const POSTS_URL =
-  "https://openapi.programming-hero.com/api/videos/category/1000";
+const POSTS_URL = "https://openapi.programming-hero.com/api/videos/category";
+
+const cartSection = document.getElementById("cartSection");
+const cameraSection = document.getElementById("cameraSection");
+const allBtn = document.getElementById("allBtn");
+const musicBtn = document.getElementById("musicBtn");
+const comedyBtn = document.getElementById("comedyBtn");
+const drawingBtn = document.getElementById("drawingBtn");
+
+const categoryButtons = [allBtn, musicBtn, comedyBtn, drawingBtn];
 
 function cardHTML(...arrData) {
   let data = {};
@@ -39,8 +47,10 @@ function cardHTML(...arrData) {
     </div>`;
 }
 
-async function getAllData() {
-  const res = await fetch(POSTS_URL);
+async function getAllData(category) {
+  cartSection.innerHTML = "";
+  cameraSection.classList.add("hidden");
+  const res = await fetch(`${POSTS_URL}/${category}`);
   const posts = await res.json();
   let authors = {};
   for (let post of posts.data) {
@@ -50,8 +60,44 @@ async function getAllData() {
 }
 
 async function showAllItems(data) {
-  const cartSection = document.getElementById("cartSection");
   cartSection.innerHTML += cardHTML(data);
 }
-getAllData();
-// showAllItems();
+
+function toggleActiveButton(selectedButton) {
+  categoryButtons.forEach((btn) => {
+    btn.classList.remove("bg-red-500", "text-white");
+    btn.classList.add("bg-gray-300", "text-white");
+  });
+
+  selectedButton.classList.remove("bg-gray-300", "text-white");
+  selectedButton.classList.add("bg-red-500", "text-white");
+}
+
+allBtn.addEventListener("click", () => {
+  toggleActiveButton(allBtn);
+  getAllData("1000"); // Load all posts
+});
+musicBtn.addEventListener("click", () => {
+  toggleActiveButton(musicBtn);
+  getAllData("1001"); // Load music category posts
+});
+comedyBtn.addEventListener("click", () => {
+  toggleActiveButton(comedyBtn);
+  getAllData("1003"); // Load comedy category posts
+});
+
+drawingBtn.addEventListener("click", () => {
+  cartSection.innerHTML = "";
+  cameraSection.innerHTML = /*html*/ `
+    <div class="flex justify-center items-center flex-col">
+          <img src="./image/camera.png" class="w-44" alt="" />
+          <h2 class="lg:text-3xl text-2xl w-72 lg:w-full mt-3 text-center font-bold">
+            Oops!! Sorry, There is no content here
+          </h2>
+    </div>
+    `;
+  cameraSection.classList.remove("hidden");
+  toggleActiveButton(drawingBtn);
+});
+
+getAllData("1000");
