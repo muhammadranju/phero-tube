d68a26a4ef8821624e9e39eaf29dcbb122ee7f05
Muhammad Ranju <mdranju23@gmail.com>
2024-10-02 12:26:50 +0600

    feat: add a modal to display post details

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ const drawingBtn = id("drawingBtn");
 const searchBtn = id("searchBtn");
 const postDetailsBtn = id("postDetailsBtn");
 const sortByBtn = id("sortByBtn");
+const modalData = id("modalData");
 
 const categoryButtons = [allBtn, musicBtn, comedyBtn, drawingBtn];
 
@@ -42,7 +43,7 @@ async function getAllData(category) {
   return allPost;
 }
 
-async function showAllItems(data) {
+function showAllItems(data) {
   cartSection.innerHTML += cardHTML(data);
 }
 
@@ -63,9 +64,11 @@ searchBtn.addEventListener("click", async () => {
     cartSection.innerHTML = "";
     cameraSection.classList.add("hidden");
     singleSection.classList.remove("hidden");
+
     const foundPost = data.find((post) =>
       post.title.includes(searchInput.value)
     );
+
     if (foundPost) {
       authorName = foundPost.authors[0];
       singleSection.innerHTML = showHTML(foundPost, authorName);
@@ -106,14 +109,14 @@ function showHTML(data, author) {
   return /*html */ `
     <div class="shadow p-5 rounded-xl relative">
           <figure class="w-full h-48">
-            <img alt="Shoes" class="rounded-xl object-cover w-full h-full"  
+            <img alt="Shoes" class="rounded-xl object-cover w-full h-full "  
               src=${data.thumbnail}                        />
           </figure>
           <!-- <span class="bg-slate-800 top-0 mt-44 right-0 mr-9 text-white pb-[3px] px-[7px] absolute rounded-lg">3hrs 56 min ago</span> -->
           <div class="flex items-center space-x-5 mt-5">
             <img src=${
               author.profile_picture
-            } class="rounded-full w-16 h-16 ml-3 " alt="" />
+            } class="rounded-full w-16 h-16 ml-3  object-cover" alt="" />
             <div class="space-y-2">
               <h2 class="card-title text-xl font-bold">${data.title}</h2>
               <div class="flex space-x-3">
@@ -129,7 +132,9 @@ function showHTML(data, author) {
             </div>
           </div>
           <div class="flex justify-center mt-2">
-            <button class="btn" id="postDetailsBtn">View Details</button>
+            <button class="btn"
+            onclick="postDetails('${data.title}')"
+            >View Details</button>
           </div>
     </div>`;
 }
@@ -144,6 +149,46 @@ function showError(title) {
     `;
 }
 
-postDetailsBtn.addEventListener("click", () => {
-  console.log(postDetailsBtn.innerHTML);
-});
+async function postDetails(slug) {
+  my_modal_5.showModal();
+  const res = await fetch(`${POSTS_URL}/1000`);
+  const data = await res.json();
+  const foundPost = data.data.find((post) => post.title.includes(slug));
+  const authors = foundPost.authors[0];
+  modalData.innerHTML = /*html*/ `
+  <figure class="w-full lg:h-96 h-80">
+              <img
+                src=${foundPost.thumbnail}
+                alt="Shoes"
+                class="object-cover w-full h-full rounded-xl"
+              />
+            </figure>
+            <div class="card-body text-center">
+              <h2 class="text-3xl font-bold text-red-500">${
+                foundPost.title
+              }</h2>
+            </div>
+            <div
+              class="flex lg:flex-row flex-col justify-center lg:gap-96 items-center"
+            >
+              <img src="${
+                authors.profile_picture
+              }" class="w-28 h-28 object-cover rounded-full" alt="" />
+              <div class="flex flex-col space-y-4">
+                <div class="flex justify-center  items-center gap-2">
+                <span class="text-lg font-semibold">${
+                  authors.profile_name
+                } </span>
+                ${
+                  authors.verified
+                    ? ' <img src="./image/icon.png" class="w-6 h-6" />'
+                    : ""
+                }
+                </div>
+                <span>${foundPost.others.views} views </span>
+              </div>
+            </div>
+  `;
+  console.log(authors);
+  console.log(foundPost);
+}
